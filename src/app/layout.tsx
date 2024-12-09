f9c95aefbdcad7f15eaffe2b7cb0f9c13a5c4488
jratul <jratul@nate.com>
2024-12-09 21:39:39 +0900

    add pretendard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -6,14 +7,21 @@ export const metadata: Metadata = {
   description: "Next.js Socket io playground",
 };
 
+const pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body>{children}</body>
+    <html lang="ko" className={`${pretendard.variable}`}>
+      <body className={"font-pretendard tracking-tight"}>{children}</body>
     </html>
   );
 }
